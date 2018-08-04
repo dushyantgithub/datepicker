cb817af8a97a6d1f74b33c4b155a02a9a7c49bec
Dushyant <chaitanya@clapglobal.com>
2018-08-04 18:40:10 +0530

    updating state changes and consoling

--- App.jsx
+++ App.jsx
@@ -13,24 +13,26 @@ class App extends React.Component {
 		  	startMonth: moment().format('MM'),
 		  	startYear: moment().subtract(1, 'y').format('YYYY'),
 	      	endYear: moment().add(1, 'y').format('YYYY'),
-	      	currentDate: moment().format('YYYY MM DD')
+	      	currentYear: moment().format('YYYY')
 		};
 		this.state.startDate = this.state.startYear + "-" + this.state.startMonth + "-" + this.state.startDay;
 		this.state.endDate = this.state.endYear + "-" + this.state.startMonth + "-" + this.state.startDay;
-		this.handleChange = this.handleChange.bind(this);
+		this.state.currentDate = this.state.currentYear + "-" + this.state.startMonth + "-" + this.state.startDay;
+		this.handleDate = this.handleDate.bind(this);
 	}
 
-	handleChange(event) {
-		this.setState({
-	      currentDate: event
-	    });
-	}
+	handleDate(event){
+	   this.setState({
+	   	currentDate: event.target.value
+	   }); 
+	};
    render() {
+   	console.log('Date', this.state.currentDate)
 	    return (
 	    	<form onSubmit={this.handleSubmit}>
 		        <label>
 			        Select Date :&nbsp;
-			        <input className="datefield" type="date" name="datefield" onChange={this.handleChange} min={this.state.startDate} max={this.state.endDate} />
+			        <input className="datefield" type="date" name="datefield" value={this.state.currentDate} onChange={this.handleDate} min={this.state.startDate} max={this.state.endDate} />
 		        </label>
 	      	</form>
 	   	);
