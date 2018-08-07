ac6b073c25c6751dd1d8ea28304c90d52729eb75
Dushyant <dushyant.singhrathore42@gmail.com>
2018-08-08 03:07:45 +0530

    new calendar in progress

--- App.jsx
+++ App.jsx
@@ -2,39 +2,76 @@ import React from 'react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 import './style.css';
-
+var dateArray = require('moment-array-dates');
 
 class App extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.dateIteration = this.dateIteration.bind(this);
 		this.state = {
 		  	startDay: moment().format('DD'),
 		  	startMonth: moment().format('MM'),
 		  	startYear: moment().subtract(1, 'y').format('YYYY'),
 	      	endYear: moment().add(1, 'y').format('YYYY'),
-	      	currentYear: moment().format('YYYY')
+	      	currentYear: moment().format('YYYY'),
+	      	dateRange: [],
+	      	addclicks: 0,
+	      	subclicks: 0,
+	      	currentMonth: 0
 		};
-		this.state.startDate = this.state.startYear + "-" + this.state.startMonth + "-" + this.state.startDay;
-		this.state.endDate = this.state.endYear + "-" + this.state.startMonth + "-" + this.state.startDay;
 		this.state.currentDate = this.state.currentYear + "-" + this.state.startMonth + "-" + this.state.startDay;
-		this.handleDate = this.handleDate.bind(this);
+		this.manipulateMonth = '';
+		this.manipulateMonth2 = '';
+		this.newDate = '';
+		this.defaultDate1 = moment().startOf('month').format('MM DD YYYY');
+	    this.defaultDate2 = moment().endOf('month').format('MM DD YYYY');
 	}
+	dateIteration(params) {
+		if(params == 'default') {
+			this.manipulateMonth = moment().add(this.state.addclicks, 'M').startOf('month').format('MM DD YYYY');
+			this.manipulateMonth2 = moment().add(this.state.addclicks, 'M').endOf('month').format('MM DD YYYY');
+			this.setState({ 
+				dateRange: dateArray.range(this.manipulateMonth, this.manipulateMonth2, 'MMM DD', true),
+				addclicks: this.state.addclicks + 0,
+				subclicks: this.state.subclicks + 0
+			});
 
-	handleDate(event){
-	   this.setState({
-	   	currentDate: event.target.value
-	   }); 
-	};
+		} else if(params == 'negative'){
+
+			this.setState({
+				subclicks: this.state.subclicks + 1,
+				addclicks: 0
+			});
+			this.manipulateMonth = moment().subtract(this.state.subclicks, 'M').startOf('month').format('MM DD YYYY');
+			this.manipulateMonth2 = moment().subtract(this.state.subclicks, 'M').endOf('month').format('MM DD YYYY');
+			this.setState({dateRange: dateArray.range(this.manipulateMonth, this.manipulateMonth2, 'MMM DD YYYY', true)});
+
+		} else if(params == 'positive') {
+
+			this.setState({
+				addclicks: this.state.addclicks + 1,
+				subclicks: 0
+			});
+			this.manipulateMonth = moment().add(this.state.addclicks, 'M').startOf('month').format('MM DD YYYY');
+			this.manipulateMonth2 = moment().add(this.state.addclicks, 'M').endOf('month').format('MM DD YYYY');
+			this.setState({dateRange: dateArray.range(this.manipulateMonth, this.manipulateMonth2, 'MMM DD YYYY', true)});
+
+		}
+		
+	}
    render() {
-   	console.log('Date', this.state.currentDate)
+   	console.log(this.state.dateRange)
+   	// console.log(this.state.startMonth)
 	    return (
-	    	<form onSubmit={this.handleSubmit}>
-		        <label>
-			        Select Date :&nbsp;
-			        <input className="datefield" type="date" name="datefield" value={this.state.currentDate} onChange={this.handleDate} min={this.state.startDate} max={this.state.endDate} />
-		        </label>
-	      	</form>
+	    	<div>
+	    		<button onClick={() => this.dateIteration('default')}>{this.state.currentDate}</button>
+	    		<div>
+	    			<button onClick={() => this.dateIteration('negative')}>Previous Month</button>
+	    			<button onClick={() => this.dateIteration('positive')}>Next Month</button>	    			
+	    		</div>
+
+	    	</div>
 	   	);
 	}
 }
